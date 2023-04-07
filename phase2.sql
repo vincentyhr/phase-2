be17565ced347b14a3526870650f2d7558d53d3c
sdubuisson7 <64994366+sdubuisson7@users.noreply.github.com>
2023-04-07 14:49:55 -0400

    all tables added, need to finish constraints

--- phase2.sql
+++ phase2.sql
@@ -2,9 +2,77 @@ CREATE DATABASE IF NOT EXISTS closeb;
 
 USE closeb;
 
+CREATE TABLE IF NOT EXISTS Carriers (
+  CarrierID INT,
+  Phone VARCHAR(20) NOT NULL,
+  Email VARCHAR(50) NOT NULL,
+  PRIMARY KEY (CarrierID)
+);
+
+CREATE TABLE IF NOT EXISTS Carrier_rates (
+  CarrierID INT NOT NULL,
+  Location VARCHAR(50) NOT NULL,
+  Price DECIMAL NOT NULL,
+  PRIMARY KEY (Location),
+  -- Carrier rates updated/deleted when carrier is
+  CONSTRAINT fk_1 FOREIGN KEY (CarrierID) REFERENCES Carriers(CarrierID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Shipments (
+  ShipmentID INT,
+  ShippingCost DECIMAL,
+  CarrierID INT,
+  OrderID INT,
+
+  -- Shipment updates if Order is updated
+  -- Can't delete order if a shipment exists for it
+  CONSTRAINT fk_2 FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT,
+  -- Shipment updates if carrier id is updated
+  -- Can't delete carrier if a shipment exists that they're carrying
+  CONSTRAINT fk_3 FOREIGN KEY (CarrierID) REFERENCES Carriers(CarrierID)
+      ON UPDATE CASCADE
+      ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+  OrderID INT,
+  Date VARCHAR(50),
+  Price DECIMAL,
+  CustomerID INT,
+  isReturn BOOL, -- foreign key for OrderID cannot reference two tables
+  PRIMARY KEY (OrderID),
+
+  -- Orders updates if Customer is updated
+  -- If Customer is deleted, all of its orders are deleted
+  CONSTRAINT fk_4 FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS OrderDetails (
+  OrderID INT,
+  ProductID INT,
+  Quantity INT,
+
+  -- If Orders is updated, Order Details is Updated
+  -- If Order is deleted, Order Details is Deleted
+  CONSTRAINT fk_5 FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  -- If Product is updated, Order Details is Updated
+  -- If Product is deleted, Order Details is Deleted
+  CONSTRAINT fk_6 FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
 
 -- Create the Customers table
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
   CustomerID INT PRIMARY KEY,
   First VARCHAR(50) NOT NULL,
   Last VARCHAR(50) NOT NULL,
@@ -15,21 +83,16 @@ CREATE TABLE Customers (
 );
 
 -- Create the Wish_lists table
-CREATE TABLE Wish_lists (
+CREATE TABLE IF NOT EXISTS Wish_lists (
   WishID INT PRIMARY KEY,
   Name VARCHAR(100) NOT NULL,
   CustomerID INT,
   FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
--- Create the Products table (assuming it exists)
-CREATE TABLE Products (
-  ProductID INT PRIMARY KEY,
-  -- Add other attributes for the Products table here
-);
 
 -- Create the Wish_item table
-CREATE TABLE Wish_item (
+CREATE TABLE IF NOT EXISTS Wish_item (
   WishID INT,
   ProductID INT,
   PRIMARY KEY (WishID, ProductID),
@@ -37,21 +100,15 @@ CREATE TABLE Wish_item (
   FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
 );
 
--- Create the Brands table (assuming it exists)
-CREATE TABLE Brands (
-  BrandID INT PRIMARY KEY,
-  -- Add other attributes for the Brands table here
-);
-
 -- Create the Social_Media table
-CREATE TABLE Social_Media (
+CREATE TABLE IF NOT EXISTS Social_Media (
   Handle VARCHAR(50) PRIMARY KEY,
   BrandID INT,
   FOREIGN KEY (BrandID) REFERENCES Brands(BrandID)
 );
 
 -- Create the Review_made table
-CREATE TABLE Review_made (
+CREATE TABLE IF NOT EXISTS Review_made (
   CustomerID INT,
   ProductID INT,
   n_stars INT,
@@ -61,7 +118,7 @@ CREATE TABLE Review_made (
   FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
 );
 
-Create Table Brands (
+CREATE TABLE IF NOT EXISTS Brands (
     BrandID INT,
     Name VARCHAR(50),
     Email VARCHAR(50),
@@ -72,7 +129,7 @@ Create Table Brands (
     PRIMARY KEY (BrandID)
 );
 
-Create Table Products (
+CREATE TABLE IF NOT EXISTS Products (
     Name VARCHAR(50),
     Description VARCHAR(50),
     ProductID INT,
@@ -84,7 +141,7 @@ Create Table Products (
     FOREIGN KEY (Category_Title) REFERENCES Categories(Title)
 );
 
-CREATE TABLE Variants (
+CREATE TABLE IF NOT EXISTS Variants (
     ProductID INT,
     color VARCHAR(50),
     size VARCHAR(50),
@@ -93,7 +150,7 @@ CREATE TABLE Variants (
     FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
 );
 
-CREATE TABLE Categories (
+CREATE TABLE IF NOT EXISTS Categories (
     Title VARCHAR(50),
     Description VARCHAR(50),
     PRIMARY KEY (Title)
